e2de0d38d83e8d87e67b91d76d4c2ce20178297c
lacava <williamlacava@gmail.com>
2017-08-14 11:35:05 -0400

    adds main

---
+++ main.cc
@@ -0,0 +1,13 @@
+#include "fewtwo.h"
+
+int main(int argc, char** argv){
+    // runs FEWTWO from the command line. 
+    Fewtwo fewtwo(); 
+    
+    std::cout << "hello i'm FEWTWO!\n";
+
+    return 0;
+
+}
+
+
