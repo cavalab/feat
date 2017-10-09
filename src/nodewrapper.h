b8636c78c67825224e66c029809b8691d4aa3f24
tilak <tilak72@gmail.com>
2017-10-09 15:29:24 -0400

    adding nodewrapper.h header file

---
+++ src/nodewrapper.h
@@ -0,0 +1,36 @@
+/* FEWTWO
+copyright 2017 William La Cava
+license: GNU/GPL v3
+*/
+#ifndef NODE_WRAPPER
+#define NODE_WRAPPER
+
+#include <memory>
+
+#include "./node/node.h"
+#include "./node/nodeadd.h"
+#include "./node/nodeand.h"
+#include "./node/nodeclosebrace.h"
+#include "./node/nodeconstant.h"
+#include "./node/nodecosine.h"
+#include "./node/nodecube.h"
+#include "./node/nodedivide.h"
+#include "./node/nodeequal.h"
+#include "./node/nodeexponent.h"
+#include "./node/nodeexponential.h"
+#include "./node/nodegreaterthan.h"
+#include "./node/nodeif.h"
+#include "./node/nodelessthan.h"
+#include "./node/nodelog.h"
+#include "./node/nodemultiply.h"
+#include "./node/nodenot.h"
+#include "./node/nodeopenbrace.h"
+#include "./node/nodeor.h"
+#include "./node/noderoot.h"
+#include "./node/nodesine.h"
+#include "./node/nodesquare.h"
+#include "./node/nodesubtract.h"
+#include "./node/nodethen.h"
+#include "./node/nodevariable.h"
+
+#endif
