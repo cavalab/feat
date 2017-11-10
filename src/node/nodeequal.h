d8ccc8d8bb6aa93a63cde40dbaddb2d1044df6d1
lacava <williamlacava@gmail.com>
2017-11-10 12:45:25 -0500

    fixes = operator

--- src/node/nodeequal.h
+++ src/node/nodeequal.h
@@ -25,16 +25,16 @@ namespace FT{
             void evaluate(const MatrixXd& X, const VectorXd& y, vector<ArrayXd>& stack_f, 
                     vector<ArrayXb>& stack_b)
             {
-            	ArrayXb x2 = stack_b.back(); stack_b.pop_back();
-                ArrayXb x1 = stack_b.back(); stack_b.pop_back();
+            	ArrayXd x2 = stack_f.back(); stack_f.pop_back();
+                ArrayXd x1 = stack_f.back(); stack_f.pop_back();
                 stack_b.push_back(x1 == x2);
             }
 
             /// Evaluates the node symbolically
             void eval_eqn(vector<string>& stack_f, vector<string>& stack_b)
             {
-            	string x2 = stack_b.back(); stack_b.pop_back();
-                string x1 = stack_b.back(); stack_b.pop_back();
+            	string x2 = stack_f.back(); stack_f.pop_back();
+                string x1 = stack_f.back(); stack_f.pop_back();
                 stack_b.push_back("(" + x1 + "==" + x2 + ")");
             }
     };
