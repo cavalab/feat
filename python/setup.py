7a5e85ee0f387b05246d3d2b7518c2a5c4158280
lacava <williamlacava@gmail.com>
2017-12-11 14:22:28 -0500

    adds setup descriptions

--- python/setup.py
+++ python/setup.py
@@ -5,4 +5,6 @@ setup(ext_modules = cythonize(
            "fewtwo.pyx",                 # our Cython source
            sources=["../src/fewtwo.h"],  # additional source file(s)
            language="c++",             # generate C++ code
+           eigency.get_includes(),
+           extra_compile_args = ['-std=c++0x','-omp']
       ))
