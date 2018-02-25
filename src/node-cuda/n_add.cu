c5729a2fece41ddac18a2f08cddec5025205015d
lacava <williamlacava@gmail.com>
2018-02-24 20:52:56 -0500

    updates n_add.h

--- src/node-cuda/n_add.cu
+++ src/node-cuda/n_add.cu
@@ -1,43 +1,67 @@
-/* FEWTWO
+/* FEAT
 copyright 2017 William La Cava
 license: GNU/GPL v3
 */
-#ifndef NODE_ADD
-#define NODE_ADD
+#ifdef USE_CUDA
 
-#include "node.h"
+//#ifndef NODE_ADD
+//#define NODE_ADD
+
+//#include "../feat.h"
+#include "nodeadd.h"
+static void HandleError( cudaError_t err, const char *file, int line )
+{
+	// CUDA error handeling from the "CUDA by example" book
+	if (err != cudaSuccess)
+    {
+		printf( "%s in %s at line %d\n", cudaGetErrorString( err ), file, line );
+		exit( EXIT_FAILURE );
+	}
+}
+
+#define HANDLE_ERROR( err ) (HandleError( err, __FILE__, __LINE__ ))
 
 namespace FT{
-	class NodeAdd : public Node
+   		
+    __global__ void Add(double * x1, double * x2, double * out, size_t N)
+    {                    
+        for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < N; i += blockDim.x * gridDim.x)
+        {
+            out[i] = x1[i] + x2[i];
+        }
+        return;
+    }
+    /// Evaluates the node and updates the stack states. 
+    void NodeAdd::evaluate(const MatrixXd& X, const VectorXd& y, vector<ArrayXd>& stack_f, 
+            vector<ArrayXb>& stack_b)
     {
-    	public:
-    	
-    		NodeAdd()
-       		{
-    			name = "+";
-    			otype = 'f';
-    			arity['f'] = 2;
-    			arity['b'] = 0;
-    			complexity = 1;
-    		}
-    		
-            /// Evaluates the node and updates the stack states. 
-            void evaluate(const MatrixXd& X, const VectorXd& y, vector<ArrayXd>& stack_f, 
-                    vector<ArrayXb>& stack_b)
-			{
-        		ArrayXd x2 = stack_f.back(); stack_f.pop_back();
-                ArrayXd x1 = stack_f.back(); stack_f.pop_back();
-                stack_f.push_back(limited(x1 + x2));
-            }
-            
-            /// Evaluates the node symbolically
-            void eval_eqn(vector<string>& stack_f, vector<string>& stack_b)
-            {
-        		string x2 = stack_f.back(); stack_f.pop_back();
-                string x1 = stack_f.back(); stack_f.pop_back();
-                stack_f.push_back("(" + x1 + "+" + x2 + ")");
-            }
-    };
-}	
+        ArrayXd x2 = stack_f.back(); stack_f.pop_back();
+        ArrayXd x1 = stack_f.back(); stack_f.pop_back();
+        // evaluate on the GPU
+        ArrayXd result = ArrayXd(x1.size());
+        size_t N = result.size();
+        double * dev_res;
+        int numSMs;
+        cudaDeviceGetAttribute(&numSMs, cudaDevAttrMultiProcessorCount, 0);
+        // allocate device arrays
+        double * dev_x1, * dev_x2 ; 
+        HANDLE_ERROR(cudaMalloc((void **)& dev_x1, sizeof(double)*N));
+        HANDLE_ERROR(cudaMalloc((void **)& dev_x2, sizeof(double)*N));
+        HANDLE_ERROR(cudaMalloc((void **)&dev_res, sizeof(double)*N));
+        // Copy to device
+        HANDLE_ERROR(cudaMemcpy(dev_x1, x1.data(), sizeof(double)*N, cudaMemcpyHostToDevice));
+        HANDLE_ERROR(cudaMemcpy(dev_x2, x2.data(), sizeof(double)*N, cudaMemcpyHostToDevice));
 
+        Add<<< 32*numSMs, 128 >>>(dev_x1, dev_x2, dev_res, N);
+       
+        // Copy to host
+        HANDLE_ERROR(cudaMemcpy(result.data(), dev_res, sizeof(double)*N, cudaMemcpyDeviceToHost));
+        
+        stack_f.push_back(limited(result));
+        // Free memory
+        cudaFree(dev_x1); cudaFree(dev_x2); cudaFree(dev_res);
+    }
+
+}	
 #endif
+
--- src/node-cuda/n_add.h
+++ src/node-cuda/n_add.h
@@ -23,13 +23,8 @@ namespace FT{
     		
             /// Evaluates the node and updates the stack states. 
             void evaluate(const MatrixXd& X, const VectorXd& y, vector<ArrayXd>& stack_f, 
-                    vector<ArrayXb>& stack_b)
-			{
-        		ArrayXd x2 = stack_f.back(); stack_f.pop_back();
-                ArrayXd x1 = stack_f.back(); stack_f.pop_back();
-                stack_f.push_back(limited(x1 + x2));
-            }
-            
+                    vector<ArrayXb>& stack_b);
+           
             /// Evaluates the node symbolically
             void eval_eqn(vector<string>& stack_f, vector<string>& stack_b)
             {
