cb49f6889719aaf54a8d53e285e66b2c0169b110
lacava <williamlacava@gmail.com>
2018-02-25 22:49:39 -0500

    adds cuta utils files

---
+++ src/node-cuda/cuda_utils.cu
@@ -0,0 +1,15 @@
+/* FEAT
+copyright 2017 William La Cava
+license: GNU/GPL v3
+*/
+static void HandleError( cudaError_t err, const char *file, int line )
+{
+	// CUDA error handeling from the "CUDA by example" book
+	if (err != cudaSuccess)
+    {
+		printf( "%s in %s at line %d\n", cudaGetErrorString( err ), file, line );
+		exit( EXIT_FAILURE );
+	}
+}
+
+
