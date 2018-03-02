a7e8a922866c586feae60cb6ad18df22c82042cc
srinu634 <srinivas.suri46@gmail.com>
2018-03-01 21:16:29 -0500

    Working SOlution

--- python/pyTest.py
+++ python/pyTest.py
@@ -1,4 +1,4 @@
-import feat
+import feat as ft
 import pyfeat
 import numpy
 from sklearn.datasets import load_boston
@@ -9,9 +9,7 @@ X = boston.data
 
 Y = boston.target
 
-clf= feat.Feat(verbosity=0)
-#clf = pyfeat.PyFeat(pop_size=100,  gens=100,  ml="LinearRidgeRegression", classification=False,  verbosity=0, max_stall=0,sel="lexicase",  surv="pareto",cross_rate=0.5,otype="a",functions="+,-,*,/,^2,^3,exp,log,and,or,not,=,<,>,ite", max_depth=3,   max_dim=10,  random_state=0, erc = False,  obj="fitness,complexity", shuffle=False,  split=0.75,  fb=0.5)
-
+clf= ft.Feat(verbosity=0)
 clf.fit(X,Y)
 
 
@@ -19,6 +17,4 @@ print clf.predict(X)
 print ( clf.transform(X))
 print clf.fit_predict(X,Y)
 print clf.fit_transform(X,Y)
-
-# pop_size=100,  gens=100,  ml="LinearRidgeRegression", classification=False,  verbosity=2,  max_stall=0,sel="lexicase",  surv="pareto",  cross_rate=0.5,otype='a',functions="+,-,*,/,^2,^3,exp,log,and,or,not,=,<,>,ite", max_depth=3,   max_dim=10,  random_state=0, erc = False,  obj="fitness,complexity", shuffle=False,  split=0.75,  fb=0.5
 
