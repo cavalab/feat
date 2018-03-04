1506cebdc96b3b3a8f8ab40a6480ec33da11baa3
srinu634 <srinivas.suri46@gmail.com>
2018-03-03 20:17:39 -0500

    Modified Parameters to self.<parameter>

--- python/feat.py
+++ python/feat.py
@@ -21,13 +21,32 @@ class Feat(BaseEstimator):
                 otype='a',  functions="+,-,*,/,^2,^3,exp,log,and,or,not,=,<,>,ite", 
                 max_depth=3,   max_dim=10,  random_state=0, 
                 erc = False,  obj="fitness,complexity", shuffle=False,  split=0.75,  fb=0.5):
-
-        self._pyfeat = pyfeat.PyFeat( pop_size,  gens,  ml, 
-                classification,  verbosity,  max_stall,
-                sel,  surv,  cross_rate,
-                otype,  functions, 
-                max_depth,   max_dim,  random_state, 
-                erc,  obj, shuffle,  split,  fb)
+        self.pop_size = pop_size
+        self.gens = gens
+        self.ml = ml
+        self.classification = classification
+        self.verbosity = verbosity
+        self.max_stall = max_stall
+        self.sel = sel
+        self.surv = surv
+        self.cross_rate = cross_rate
+        self.otype = otype
+        self.functions = functions
+        self.max_depth = max_depth
+        self.max_dim = max_dim
+        self.random_state = random_state
+        self.erc = erc      
+        self.obj = obj
+        self.shuffle = shuffle
+        self.split = split
+        self.fb = fb
+   
+        self._pyfeat = pyfeat.PyFeat( self.pop_size,  self.gens,  self.ml, 
+                self.classification,  self.verbosity,  self.max_stall,
+                self.sel,  self.surv,  self.cross_rate,
+                self.otype,  self.functions, 
+                self.max_depth,   self.max_dim,  self.random_state, 
+                self.erc,  self.obj, self.shuffle,  self.split,  self.fb)
 
     def fit(self,X,y):
         self._pyfeat.fit(X,y)
@@ -50,6 +69,6 @@ def main():
     parser = argparse.ArgumentParser(description="A feature engineering wrapper for ML.",
                                      add_help=False)
 
-    clf = Feat()
+    
 if __name__ == '__main__':
     main()
