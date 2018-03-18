dd8c9e42f59950974c0fba3a707b2509522a2f36
srinu634 <srinivas.suri46@gmail.com>
2018-03-18 19:05:35 -0400

    Revert "standardizing the travis files to follow a common format"
    This reverts commit f7128d922f17c851f548ff7bf17b70f7eacbd682.

--- ci/.travis_install.sh
+++ ci/.travis_install.sh
@@ -58,6 +58,6 @@ echo "running feat.."
 cd ./python
 sudo python setup.py install build_ext --inplace
 
-#_____Copy wrapper tests to python folder_____#
+#_____Run the Python Tests for the wrapper_____#
 cd ../tests
 sudo cp wrappertest.py ../python/ #Copy the file to python folder
