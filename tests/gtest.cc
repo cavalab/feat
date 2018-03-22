ddf5f89be2ca7e78fcf0267de162322006d89f94
lacava <williamlacava@gmail.com>
2018-03-22 14:56:20 -0400

    updates evaluation object in EvalEquation test

--- tests/gtest.cc
+++ tests/gtest.cc
@@ -270,7 +270,8 @@ TEST(Individual, EvalEquation)
             
     feat.p_ml = make_shared<ML>(feat.params); // intialize ML
     feat.p_pop = make_shared<Population>(feat.params.pop_size);
-    
+    feat.p_eval = make_shared<Evaluation>(feat.params.scorer);
+
 	feat.params.set_terminals(X.rows()); 
         
     // initial model on raw input
