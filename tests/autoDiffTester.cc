37718df65b67375adf9292b3b2078b0d381047e5
JPT2 <jtagg@seas.upenn.edu>
2018-04-09 21:46:15 -0400

    Updated the include search directive

--- tests/autoDiffTester.cc
+++ tests/autoDiffTester.cc
@@ -2,24 +2,30 @@
 #include <iostream>
 #include <string>
 #include <stack>
-#include <../src/node/node.h>
-#include <../src/node/nodeDx.h>
-#include <../src/node/nodeadd.h>
-#include <../src/node/nodecos.h>
-#include <../src/node/nodecube.h>
-#include <../src/node/nodedivide.h>
-#include <../src/node/nodeexponent.h>
-#include <../src/node/nodeexponential.h>
-#include <../src/node/nodegaussian.h>
-#include <../src/node/nodelog.h>
-#include <../src/node/nodelogit.h>
-#include <../src/node/nodemultiply.h>
-#include <../src/node/noderelu.h>
-#include <../src/node/noderoot.h>
-#include <../src/node/nodesqrt.h>
-#include <../src/node/nodesquare.h>
-#include <../src/node/nodesubtract.h>
-#include <../src/node/nodetanh.h>
+#include <Eigen/Dense>
+
+// Include node and node children
+#include "../src/node/node.h"
+#include "../src/node/nodeDx.h"
+#include "../src/node/nodeadd.h"
+#include "../src/node/nodecos.h"
+#include "../src/node/nodecube.h"
+#include "../src/node/nodedivide.h"
+#include "../src/node/nodeexponent.h"
+#include "../src/node/nodeexponential.h"
+#include "../src/node/nodegaussian.h"
+#include "../src/node/nodelog.h"
+#include "../src/node/nodelogit.h"
+#include "../src/node/nodemultiply.h"
+#include "../src/node/noderelu.h"
+#include "../src/node/noderoot.h"
+#include "../src/node/nodesqrt.h"
+#include "../src/node/nodesquare.h"
+#include "../src/node/nodesubtract.h"
+#include "../src/node/nodetanh.h"
+
+// Backprop progam
+#include "../src/auto_backprop.h"
 
 
 using namespace std;
@@ -30,7 +36,7 @@ using Eigen::VectorXd;
 typedef Eigen::Array<bool,Eigen::Dynamic,1> ArrayXb;
 
 int main() {
-	testNodes();
+	// testNodes();
 	return 1;
 }
 
@@ -82,10 +88,58 @@ int testNodes() {
 	if (toTest.getDerivative != expectedDerivative) { // Currently pseudocode
 		std::cout << "Cube node failed!\n";
 	}
+
+	toTest = new NodeExponential();
+	expectedDerivative = NULL;
+	if (toTest.getDerivative != expectedDerivative) { // Currently pseudocode
+		std::cout << "Exponential node failed!\n";
+	}
+
+	toTest = new NodeGaussian();
+	expectedDerivative = NULL;
+	if (toTest.getDerivative != expectedDerivative) { // Currently pseudocode
+		std::cout << "Gaussian node failed!\n";
+	}
+
+	toTest = new NodeLog();
+	expectedDerivative = NULL;
+	if (toTest.getDerivative != expectedDerivative) { // Currently pseudocode
+		std::cout << "Log node failed!\n";
+	}
+
+	toTest = new NodeLogit();
+	expectedDerivative = NULL;
+	if (toTest.getDerivative != expectedDerivative) { // Currently pseudocode
+		std::cout << "Logit node failed!\n";
+	}
+
+	toTest = new NodeRelu();
+	expectedDerivative = NULL;
+	if (toTest.getDerivative != expectedDerivative) { // Currently pseudocode
+		std::cout << "Relu node failed!\n";
+	}
+
+	toTest = new NodeSqrt();
+	expectedDerivative = NULL;
+	if (toTest.getDerivative != expectedDerivative) { // Currently pseudocode
+		std::cout << "Sqrt node failed!\n";
+	}
+
+	toTest = new NodeSquare();
+	expectedDerivative = NULL;
+	if (toTest.getDerivative != expectedDerivative) { // Currently pseudocode
+		std::cout << "Square node failed!\n";
+	}
+
+	toTest = new NodeTanh();
+	expectedDerivative = NULL;
+	if (toTest.getDerivative != expectedDerivative) { // Currently pseudocode
+		std::cout << "Tanh node failed!\n";
+	}
 }
 
 int testDummyProgram() {
-
+	
 }
 
 int testSimpleBackProp() {
