da0fc2795acffc7e304876a13f82e9077faaa459
Tilak Raj Singh <tilak72@gmail.com>
2018-04-23 14:06:04 -0400

    bug in eval_eqn pop twice

--- src/node/nodemedian.h
+++ src/node/nodemedian.h
@@ -43,7 +43,6 @@ namespace FT{
             /// Evaluates the node symbolically
             void eval_eqn(Stacks& stack)
             {
-                string x1 = stack.zs.pop();
                 stack.fs.push("median(" + stack.zs.pop() + ")");
             }
         protected:
