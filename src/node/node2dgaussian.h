fbce4cc4f030997d998a561539cdcddd0d616499
Tilak Raj Singh <tilak72@gmail.com>
2018-05-02 01:28:42 -0400

    node pop sequence

--- src/node/node2dgaussian.h
+++ src/node/node2dgaussian.h
@@ -26,8 +26,8 @@ namespace FT{
                           const std::map<string, std::pair<vector<ArrayXd>, vector<ArrayXd> > > &Z, 
 			              Stacks& stack)
             {
-        		ArrayXd x2 = stack.f.pop();
-                ArrayXd x1 = stack.f.pop();
+        		ArrayXd x1 = stack.f.pop();
+                ArrayXd x2 = stack.f.pop();
                 
                 stack.f.push(limited(exp(-1*(pow((x1-x1.mean()), 2)/(2*variance(x1)) 
                                   + pow((x2 - x2.mean()), 2)/variance(x2)))));
@@ -36,8 +36,8 @@ namespace FT{
             /// Evaluates the node symbolically
             void eval_eqn(Stacks& stack)
             {
-        		string x2 = stack.fs.pop();
-                string x1 = stack.fs.pop();
+        		string x1 = stack.fs.pop();
+                string x2 = stack.fs.pop();
                 stack.fs.push("gauss2d(" + x1 + "," + x2 + ")");
             }
 
