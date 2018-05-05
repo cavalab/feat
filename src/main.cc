7a972b7dc5fdd4269ad8bf5c42b86dd3a4d7f3e1
lacava <williamlacava@gmail.com>
2018-05-05 16:17:51 -0400

    debugging get_weights leak

--- src/main.cc
+++ src/main.cc
@@ -96,6 +96,7 @@ int main(int argc, char** argv){
         cout << "-isplit\tInternal slit for Feat's training procedure (0.75)\n";
         cout << "-f\tfeedback strength of ML on variation probabilities (0.5)\n";
         cout << "-n\tname to append to files\n";
+        cout << "-n_threads\tmaximum number of threads\n";
         cout << "-ldata\tpath to longitudinal data file\n";
         cout << "-scorer\tscoring function [mse, zero_one, bal_zero_one, log, multi_log]\n"; 
         cout << "-h\tDisplay this help message and exit.\n";
@@ -150,6 +151,8 @@ int main(int argc, char** argv){
         ldataFile = input.getCmdOption("-ldata");
     if(input.cmdOptionExists("-scorer"))
         feat.set_scorer(input.getCmdOption("-scorer"));
+    if(input.cmdOptionExists("-n_threads"))
+        feat.set_n_threads(std::stoi(input.getCmdOption("-n_threads")));
     //cout << "done.\n";
     ///////////////////////////////////////
 
