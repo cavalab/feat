825cfb48f23d24cb904fb42eecd2722a53876dcb
lacava <williamlacava@gmail.com>
2018-05-09 13:41:03 -0400

    updates python wrapper for batch settings

--- python/pyfeat.pyx
+++ python/pyfeat.pyx
@@ -23,7 +23,7 @@ cdef extern from "feat.h" namespace "FT":
                unsigned int max_depth, unsigned int max_dim, int random_state, 
                bool erc, string obj,bool shuffle, 
                double split, double fb, string scorer, 
-               string feature_names, bool backprop, int iters, double lr,
+               string feature_names, bool backprop, int iters, double lr, int bs,
                int n_threads) except + 
         void fit(double * X, int rowsX, int colsX, double*  y , int lenY)
         VectorXd predict(double * X, int rowsX,int colsX)
@@ -46,7 +46,7 @@ cdef class PyFeat:
                   int max_stall,string sel, string surv, float cross_rate,string otype, 
                   string functions, unsigned int max_depth, unsigned int max_dim, 
                   int random_state, bool erc , string obj,bool shuffle, double split, double fb,
-                  string scorer, string feature_names, bool backprop, int iters, double lr, 
+                  string scorer, string feature_names, bool backprop, int iters, double lr, int bs,
                   int n_threads):
         cdef char otype_char
         if ( len(otype) == 0):
@@ -55,7 +55,7 @@ cdef class PyFeat:
             otype_char = ord(otype)
         self.ft = Feat(pop_size,gens,ml,classification,verbosity,max_stall,sel,surv,cross_rate,
         otype_char, functions, max_depth, max_dim, random_state, erc, obj, shuffle, split, fb, scorer,
-        feature_names, backprop, iters, lr, n_threads)
+        feature_names, backprop, iters, lr, bs, n_threads)
 
     def fit(self,np.ndarray X,np.ndarray y):
         cdef np.ndarray[np.double_t, ndim=2, mode="fortran"] arr_x
