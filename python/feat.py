5323537c0e7feaeab3be6f29311e8c6e3c3e2cdc
lacava <williamlacava@gmail.com>
2018-05-09 13:41:03 -0400

    updates python wrapper for batch settings
    Former-commit-id: dca108da332bea8806ccbf5e9af215164cacebde [formerly a638a0459047f611b6ecde158d47506a9fadf012] [formerly f0f3f9ffac4306a0bd04e06cca9228f9f917649f [formerly 825cfb48f23d24cb904fb42eecd2722a53876dcb]]
    Former-commit-id: f04d2cca5e79221fd33a583843fce5bd8f11cc77 [formerly 40c64554422014a80ca517e4838176099f0c4105]
    Former-commit-id: 5ddec13b43ee9669fa1aa2443ae208edf0121f8a

--- python/feat.py
+++ python/feat.py
@@ -24,7 +24,7 @@ class Feat(BaseEstimator):
                 otype ='a',  functions ="", 
                 max_depth=3,   max_dim=10,  random_state=0, 
                 erc = False,  obj ="fitness,complexity", shuffle=False,  split=0.75,  fb=0.5,
-                scorer ='',feature_names="", backprop=False, iters=10, lr=0.1, n_threads=0):
+                scorer ='',feature_names="", backprop=False, iters=10, lr=0.1, bs=100, n_threads=0):
         self.pop_size = pop_size
         self.gens = gens
         self.ml = ml.encode() if( isinstance(ml,str) )  else ml
@@ -51,6 +51,7 @@ class Feat(BaseEstimator):
         self.backprop = backprop
         self.iters = iters
         self.lr = lr
+        self.batch_size= bs
 
         self._pyfeat = pyfeat.PyFeat( self.pop_size,  self.gens,  self.ml, 
                 self.classification,  self.verbosity,  self.max_stall,
@@ -67,6 +68,7 @@ class Feat(BaseEstimator):
                 self.backprop,
                 self.iters,
                 self.lr,
+                self.batch_size,
                 self.n_threads)
 
     def fit(self,X,y,zfile=None,zids=None):
