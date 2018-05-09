567b669c4d7870d91861543f1ead0eec17622ee7
lacava <williamlacava@gmail.com>
2018-05-09 14:56:27 -0400

    updates misisng docs

---
+++ docs/d3/dd7/classFT_1_1AutoBackProp.js
@@ -0,0 +1,21 @@
+var classFT_1_1AutoBackProp =
+[
+    [ "BP_NODE", "d6/dd3/structFT_1_1AutoBackProp_1_1BP__NODE.html", "d6/dd3/structFT_1_1AutoBackProp_1_1BP__NODE" ],
+    [ "callback", "d3/dd7/classFT_1_1AutoBackProp.html#a4ff101ebdbb11f3f86b1d5ee721f1735", null ],
+    [ "AutoBackProp", "d3/dd7/classFT_1_1AutoBackProp.html#a410e766344c9c94c2c71d7e71d1bde88", null ],
+    [ "backprop", "d3/dd7/classFT_1_1AutoBackProp.html#ad6701296b4a1c425d7cdeefe077cb7b4", null ],
+    [ "forward_prop", "d3/dd7/classFT_1_1AutoBackProp.html#a80c7f20d8c56a4ff90aab32d4012d9df", null ],
+    [ "get_batch", "d3/dd7/classFT_1_1AutoBackProp.html#a07a0a19c49793a9024ece311a7e9f2fe", null ],
+    [ "next_branch", "d3/dd7/classFT_1_1AutoBackProp.html#a12a6632f4326107e5cee430fb720a996", null ],
+    [ "pop", "d3/dd7/classFT_1_1AutoBackProp.html#a2a4cef3167ff54964e8ab8b4ac5afc4a", null ],
+    [ "pop_front", "d3/dd7/classFT_1_1AutoBackProp.html#a29399d81d427a2219d95e8999df3f10f", null ],
+    [ "print_weights", "d3/dd7/classFT_1_1AutoBackProp.html#a232ee4e6f4095308657870814267e263", null ],
+    [ "run", "d3/dd7/classFT_1_1AutoBackProp.html#a967e7e0a767799d149e9ea431ff2a1cd", null ],
+    [ "a", "d3/dd7/classFT_1_1AutoBackProp.html#a1cc7e22ca4668e6b2fc45a5bfe1c0f60", null ],
+    [ "cost_func", "d3/dd7/classFT_1_1AutoBackProp.html#a2fcf1295b8f5bb58cc96fde90a66e0c8", null ],
+    [ "d_cost_func", "d3/dd7/classFT_1_1AutoBackProp.html#a39eb8c5249a2aaffe6d178963cb5903c", null ],
+    [ "d_score_hash", "d3/dd7/classFT_1_1AutoBackProp.html#a094e12d7922cea290a2a0a139380d31e", null ],
+    [ "iters", "d3/dd7/classFT_1_1AutoBackProp.html#a9e76f2037f0b31114485938f58c6501f", null ],
+    [ "n", "d3/dd7/classFT_1_1AutoBackProp.html#aa17710c610e6bd7e4132f32a955a2b85", null ],
+    [ "score_hash", "d3/dd7/classFT_1_1AutoBackProp.html#a6954333171cb1af0cbd29a4f523b9bbe", null ]
+];
