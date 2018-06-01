102817cc38c27942f32c0170933c50ba564537d6
lacava <williamlacava@gmail.com>
2018-06-01 10:58:11 -0400

    cleans up gauss2d
    Former-commit-id: 7fb5e912c95fd4eaad63fc1e99c3231e3625fc87 [formerly 801e101ed9d3d9f519fe375d046ce90927472904] [formerly 6f5b615fd61f3f86bff75ad8320dfa126a9472b7 [formerly ffbc6caaba1792896743c605ce18f769f0784331]]
    Former-commit-id: 0cc689c3fbf40abf02ceb4c43572908967f999cc [formerly 22ea3c1ae9daf58deb7bc745c82f031d50ee903d]
    Former-commit-id: 453c8362fab26b0e6b27960273785444d9968961

--- src/node/node2dgaussian.h
+++ src/node/node2dgaussian.h
@@ -41,15 +41,16 @@ namespace FT{
                 stack.fs.push("gauss2d(" + stack.fs.pop() + "," + stack.fs.pop() + ")");
             }
 
-            ArrayXd getDerivative(vector<ArrayXd>& stack_f, int loc) {
-                // TODO 
-                
+            ArrayXd getDerivative(vector<ArrayXd>& stack_f, int loc) 
+            {
                 switch (loc) {
                     case 1: // d/dw0
-                        return -2 * W[0] * pow(stack_f[stack_f.size() - 1], 2) * exp(-pow(W[0] * stack_f[stack_f.size() - 1], 2));
+                        return (-2 * W[0] * pow(stack_f[stack_f.size() - 1], 2) 
+                                * exp(-pow(W[0] * stack_f[stack_f.size() - 1], 2)));
                     case 0: // d/dx0
                     default:
-                        return -2 * pow(W[0], 2) * stack_f[stack_f.size() - 1] * exp(-pow(W[0] * stack_f[stack_f.size() - 1], 2));
+                        return (-2 * pow(W[0], 2) * stack_f[stack_f.size() - 1] 
+                                * exp(-pow(W[0] * stack_f[stack_f.size() - 1], 2)));
                 } 
             }
             
