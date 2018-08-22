aa9c70383b3ed76337e34d3c954abdf1a314dc61
lacava <williamlacava@gmail.com>
2018-08-22 17:40:07 -0400

    some cleanup; reworks install file to allow multiple arguments; passes data by reference to fitness() and val_fitness()

--- src/feat.cc
+++ src/feat.cc
@@ -371,7 +371,8 @@ void Feat::fit(MatrixXd& X, VectorXd& y,
 
     // start the clock
     timer.Reset();
-
+    if (params.use_batch)
+        cout << "using batch with batch_size= " << params.bp.batch_size << "\n";
     std::ofstream log;                      ///< log file stream
     if (!logfile.empty())
         log.open(logfile, std::ofstream::app);
@@ -421,8 +422,8 @@ void Feat::fit(MatrixXd& X, VectorXd& y,
     params.set_terminals(d.o->X.rows(), d.o->Z);        
 
     // initial model on raw input
-    params.msg("Fitting initial model", 2);
-    
+    params.msg("Setting up data", 2);
+    float t0 =  timer.Elapsed().count();
     //data for batch training
     MatrixXd Xb;
     VectorXd yb;
@@ -440,9 +441,12 @@ void Feat::fit(MatrixXd& X, VectorXd& y,
     
     if (params.classification) 
         params.set_sample_weights(d.t->y); 
-    
+    cout << "time elapsed: " << timer.Elapsed().count() - t0 << "sec\n";
+    params.msg("Fitting initial model", 2);
+
+    t0 =  timer.Elapsed().count();
     initial_model(d);  
-    
+    params.msg(std::to_string(timer.Elapsed().count() - t0) + " seconds",2);
     // initialize population 
     params.msg("Initializing population", 2);
    
@@ -461,16 +465,11 @@ void Feat::fit(MatrixXd& X, VectorXd& y,
     p_eval->fitness(p_pop->individuals,*d.t,F,params);
     
     params.msg("Initial population done",2);
+    params.msg(std::to_string(timer.Elapsed().count()) + " seconds",2);
     
     vector<size_t> survivors;
     
-    //data for batch training
-    /*MatrixXd Xb;
-    VectorXd yb;
-    std::map<string, std::pair<vector<ArrayXd>, vector<ArrayXd> > > Zb;
-    Data db(Xb, yb, Zb, params.classification);*/
-    
-    if(params.use_batch)
+    if(params.use_batch)    // reset d to all training data
         d.setTrainingData(tmp_train, true);
 
     // =====================
@@ -487,7 +486,7 @@ void Feat::fit(MatrixXd& X, VectorXd& y,
         if(params.use_batch)
         {
             d.t->get_batch(db, params.bp.batch_size);
-            DataRef dbr;
+            DataRef dbr;    // reference to minibatch data
             dbr.setTrainingData(&db);
             
             if (params.classification)
@@ -508,12 +507,27 @@ void Feat::fit(MatrixXd& X, VectorXd& y,
     // evaluate population on validation set
     if (params.split < 1.0)
     {
+        vector<Individual>& final_pop = use_arch ? arch.archive : p_pop->individuals; 
         F_v.resize(d.v->X.cols(),int(2*params.pop_size)); 
-        if (use_arch){
-            p_eval->val_fitness(arch.archive, *d.t, F_v, *d.v, params);
+        
+        if(params.use_batch)
+        {
+            cout << "getting batch...\n";
+            t0 =  timer.Elapsed().count();
+            d.t->get_batch(db, params.bp.batch_size);
+            DataRef dbr;    // reference to minibatch data
+            dbr.setTrainingData(&db);
+            
+            if (params.classification)
+                params.set_sample_weights(dbr.t->y); 
+            cout << "got batch (" << timer.Elapsed().count() - t0 << " sec). val_fitness...\n";
+            t0 = timer.Elapsed().count();
+            p_eval->val_fitness(final_pop, *dbr.t, F_v, *d.v, params);
+            cout << "val_fitness completed in " << timer.Elapsed().count() - t0 << " seconds\n";
         }
         else
-            p_eval->val_fitness(p_pop->individuals, *d.t, F_v, *d.v, params);
+            p_eval->val_fitness(final_pop, *d.t, F_v, *d.v, params);
+
         update_best(true);                  // get the best validation model
     }
     else
