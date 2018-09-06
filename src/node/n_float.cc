75ddaf965929e5482d5908435f744c9202d6546f
Tilak Raj Singh <tilak72@gmail.com>
2018-09-05 21:50:33 -0400

    template class n_float
    Former-commit-id: cbcb81e0ddba03e98b079741d51b518786b2a827 [formerly 95170cda7aaa4e29a340a7d68c056c2e5c3503b2] [formerly 195013936581c335b4fe36dd87f56e5f318f8520 [formerly b954b6b4e6eb142a2fb4f734446a9060db566887]]
    Former-commit-id: 98a00f1b1b15e82f03ed8760d078463f50d0353b [formerly 0ee1d7b56cec21f26cec6d66b46376fd6ade11f4]
    Former-commit-id: 52714b511a115eebcf973ef62862fd0858e25a3d

--- src/node/n_float.cc
+++ src/node/n_float.cc
@@ -6,39 +6,55 @@ license: GNU/GPL v3
     	
 namespace FT{
 
-    NodeFloat::NodeFloat(bool isCategorical)
+    template <>
+    NodeFloat<double>::NodeFloat()
     {
-        if(!isCategorical)
-        {
-	        name = "f";
-	        otype = 'f';
-	    }
-	    else
-	    {
-	        name = "f_c";
-            otype = 'c';
-	    }
-	    
+        name = "f";
+        otype = 'f';
+    
+        arity['b'] = 1;
+        complexity = 1;
+    }
+    
+    template <>
+    NodeFloat<int>::NodeFloat()
+    {
+        name = "f_c";
+        otype = 'c';
+    
         arity['b'] = 1;
         complexity = 1;
     }
 
     /// Evaluates the node and updates the stack states. 
-    void NodeFloat::evaluate(const Data& data, Stacks& stack)
+    template <>
+    void NodeFloat<double>::evaluate(const Data& data, Stacks& stack)
     {
-        if(otype == 'f')
-            stack.push<double>(stack.pop<bool>().cast<double>());
-        else
-            stack.push<int>(stack.pop<bool>().cast<int>());
+        stack.push<double>(stack.pop<bool>().cast<double>());
+    }
+    
+    template <>
+    void NodeFloat<int>::evaluate(const Data& data, Stacks& stack)
+    {
+        stack.push<int>(stack.pop<bool>().cast<int>());
     }
 
     /// Evaluates the node symbolically
-    void NodeFloat::eval_eqn(Stacks& stack)
+    template <>
+    void NodeFloat<double>::eval_eqn(Stacks& stack)
     {
         stack.push<double>("f(" + stack.popStr<bool>() + ")");
     }
     
-    NodeFloat* NodeFloat::clone_impl() const { return new NodeFloat(*this); }
+    template <>
+    void NodeFloat<int>::eval_eqn(Stacks& stack)
+    {
+        stack.push<int>("f_c(" + stack.popStr<bool>() + ")");
+    }
+    
+    template <class T>
+    NodeFloat<T>* NodeFloat<T>::clone_impl() const { return new NodeFloat<T>(*this); }
 
-    NodeFloat* NodeFloat::rnd_clone_impl() const { return new NodeFloat(); }  
+    template <class T>
+    NodeFloat<T>* NodeFloat<T>::rnd_clone_impl() const { return new NodeFloat<T>(); }  
 }
