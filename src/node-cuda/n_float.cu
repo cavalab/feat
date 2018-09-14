3bdf362f92f1edea8e17754807758c20256e27c5
Tilak Raj Singh <tilak72@gmail.com>
2018-09-14 15:59:16 -0400

    fixing tests
    Former-commit-id: 3a45814339574d72e2ba17e3499e18465c3d8cd9 [formerly 0470d32ba1703d623b2b20de12fb97dfb0618d50] [formerly 8249b91e6e21317825c4a54e25e0207ccc0bb76c [formerly 6135c960d3a88f4007b06f4cd3d6a9c30b565076]]
    Former-commit-id: 7ddcff3bc89121d13821b7154321a5f57bc2d513 [formerly 036a51327b85cd5928fdf151475e2fd372ccc8b1]
    Former-commit-id: 64954b1a45d3f565559cecd9c91655826a6464e9

--- src/node-cuda/n_float.cu
+++ src/node-cuda/n_float.cu
@@ -11,7 +11,7 @@ namespace FT{
     __global__ void Float(float * x, bool* y, size_t idxf, size_t idxb, size_t N)
     {                    
 	    for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < N; i += blockDim.x * gridDim.x)
-            x[(idx)*N + i] = float(y[(idx-1)*N + i]);
+            x[(idxf)*N + i] = float(y[(idxb-1)*N + i]);
             
         return;
     }
@@ -26,12 +26,12 @@ namespace FT{
     
     void GPU_Float(float * x, bool* y, size_t idxf, size_t idxb, size_t N)
     {
-        Add<<< DIM_GRID, DIM_BLOCK >>>(x, y, idxf, idxb, N);
+        Float<<< DIM_GRID, DIM_BLOCK >>>(x, y, idxf, idxb, N);
     }
     
-    void GPU_Float(float * x, int* y, size_t idxf, size_t idxf, size_t N)
+    void GPU_Float(float * x, int* y, size_t idxf, size_t idxi, size_t N)
     {
-        Add<<< DIM_GRID, DIM_BLOCK >>>(x, y, idxf, idxi, N);
+        Float<<< DIM_GRID, DIM_BLOCK >>>(x, y, idxf, idxi, N);
     }
 }	
 
