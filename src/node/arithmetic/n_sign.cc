fff00869f17cbe9a52e41fc683e150de5f9b424a
lacava <williamlacava@gmail.com>
2018-10-16 16:39:17 -0400

    updates backprop tests with assertions and printouts
    Former-commit-id: 7d99e6b00c1b4d7f15017a00285fc701323bf2d4 [formerly f17bffb24f73241fc1bf93daa665a469b82b325f] [formerly c728f160c3caf945c61bf8ad86050294c9ebe802 [formerly c9f5ebd1d588c7baeabfe897550f24e0ba0f9703]]
    Former-commit-id: 9e2fed7c2ce4fbd4417a165abba7ed923be29918 [formerly 3edf68053736b6231688f069d9c03af2f70e94c9]
    Former-commit-id: b15930a7ff87a5ee79c62fe9b3df5ae78a60e3b2

--- src/node/arithmetic/n_sign.cc
+++ src/node/arithmetic/n_sign.cc
@@ -6,21 +6,13 @@ license: GNU/GPL v3
 #include "n_sign.h"
 
 namespace FT{
-    NodeSign::NodeSign(vector<double> W0)
+    NodeSign::NodeSign()
     {
         name = "sign";
 	    otype = 'f';
 	    arity['f'] = 1;
 	    complexity = 1;
 
-        if (W0.empty())
-        {
-            for (int i = 0; i < arity['f']; i++) {
-                W.push_back(r.rnd_dbl());
-            }
-        }
-        else
-            W = W0;
     }
 
     /// Evaluates the node and updates the stack states. 
@@ -29,7 +21,7 @@ namespace FT{
 	    ArrayXd x = stack.pop<double>();
         ArrayXd ones = ArrayXd::Ones(x.size());
 
-	    ArrayXd res = (W[0] * x > 0).select(ones, 
+	    ArrayXd res = ( x > 0).select(ones, 
                                             (x == 0).select(ArrayXd::Zero(x.size()), 
                                                             -1*ones)); 
         stack.push<double>(res);
@@ -41,19 +33,6 @@ namespace FT{
         stack.push<double>("sign("+ stack.popStr<double>() +")");
     }
 
-    ArrayXd NodeSign::getDerivative(Trace& stack, int loc)
-    {
-        ArrayXd& x = stack.get<double>()[stack.size<double>()-1];
-        // Might want to experiment with using a perceptron
-        // update rule or estimating with some other function
-        switch (loc) {
-            case 1: // d/dw0
-                return x / (2 * sqrt(W[0] * x));
-            case 0: // d/dx0
-            default:
-                return W[0] / (2 * sqrt((W[0] * x.abs())));
-        } 
-    }
     
     NodeSign* NodeSign::clone_impl() const { return new NodeSign(*this); }
 
