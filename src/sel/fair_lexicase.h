48a0158579737511fbb530542fad985315bcee09
lacava <williamlacava@gmail.com>
2019-12-09 15:27:49 -0500

    adds fair lexicase files
    Former-commit-id: c68a9d772b6adc3325299da26ecf8aa589e84411 [formerly 6bdaccebbc291259b67f9de230c7abb2875fa878] [formerly d48ab1f8958c862d58880f31f5839e8408ad08b3 [formerly 6c13928bc4c8738c0ef4f1efb218115d317dc157]]
    Former-commit-id: d28689838dc83f7f5097625ccc2611c7b7e91cb2 [formerly 4056395c6a8f95e9ccb7f28f3e9b6a35c35b291e]
    Former-commit-id: 951c7a99d091c4512a780b0725d04c9f839312c7

---
+++ src/sel/fair_lexicase.h
@@ -0,0 +1,39 @@
+/* FEWTWO
+copyright 2017 William La Cava
+license: GNU/GPL v3
+*/
+
+#ifndef LEXICASE_H
+#define LEXICASE_H
+
+#include "selection_operator.h"
+
+
+namespace FT{
+
+    namespace Sel{
+        ////////////////////////////////////////////////////////////////////////////////// Declarations
+        /*!
+         * @class Lexicase
+         * @brief Lexicase selection operator.
+         */
+        struct Lexicase : SelectionOperator
+        {
+            Lexicase(bool surv);
+            
+            ~Lexicase();
+
+            /// function returns a set of selected indices from F. 
+            vector<size_t> select(Population& pop, const MatrixXf& F, 
+                    const Parameters& params, const Data& d); 
+            
+            /// lexicase survival
+            vector<size_t> survive(Population& pop, const MatrixXf& F, 
+                    const Parameters& params, const Data& d); 
+
+        };
+    }
+
+}
+
+#endif
