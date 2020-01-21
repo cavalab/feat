bb857fe2e75da519d76e8ee34e4e7329baeae3a2
lacava <williamlacava@gmail.com>
2020-01-21 10:24:10 -0500

    attempt at pickling cython
    Former-commit-id: 0ab4f0672198dabbfad0db69ae94671df5c4fb53 [formerly a9d2d80ad19428fcb2e8408bb163165b5afbe4df] [formerly c20ed9f1a7579a844937056c49db014ab80d006e [formerly 509dd877d3f6a209080a612d698df162cea3894b]]
    Former-commit-id: 8e535157776697d09187b377fad1241824e8a297 [formerly 1f1f1978bb15d992dce86fd886da561848a8655c]
    Former-commit-id: e30da9158d214044c8aedf762767c9dfb83ed32c

--- tests/wrappertest.py
+++ tests/wrappertest.py
@@ -10,6 +10,7 @@ import argparse
 import sys
 import pandas as pd
 import numpy as np
+import pickle
 verbosity = 2
 
 class TestFeatWrapper(unittest.TestCase):
@@ -110,6 +111,15 @@ class TestFeatWrapper(unittest.TestCase):
         for key in self.clf.stats:
             print("Length for ", key, "is ", len(self.clf.stats[key]))
             self.assertEqual(len(self.clf.stats[key]), self.clf.gens)
+
+    #Test ability to pickle feat model
+    def test_pickling(self):
+        self.debug("Pickle Feat object")
+        pickle.dump(self.clf, 'test_pickle.pkl')
+
+        loaded_clf = pickle.load('test_pickle.pkl')
+
+        assert(loaded_clf.get_params() == self.clf.get_params())
 
 
 if __name__ == '__main__':
