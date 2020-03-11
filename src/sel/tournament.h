37c10a2d5f0ed3638f3c87c8bb62306e1c793018
lacava <williamlacava@gmail.com>
2020-03-11 10:25:28 -0400

    adds tournament
    Former-commit-id: 4c8fdfa4b494a9d6e13a65fdfb57a161683651e3 [formerly 92479ddd5364a4ed967a7f1294eeb637019a9c96] [formerly d3963a08f74987b363ffbca5eca191fd3e7c941a [formerly 3f0b1bb614d0d4aadced904ecd008123e8eccb94]]
    Former-commit-id: bfe7d54c1e78e8d6e790f821133912fadec2fecb [formerly 673a46b25a333448571c032f71c111adb6395d9c]
    Former-commit-id: 408ca5b364d667ed6eb94f5cea31f053cd8fd1cc

---
+++ src/sel/tournament.h
@@ -0,0 +1,41 @@
+/* FEWTWO
+copyright 2017 William La Cava
+license: GNU/GPL v3
+*/
+#ifndef TOURNAMENT_H
+#define TOURNAMENT_H
+
+#include "selection_operator.h"
+
+namespace FT{
+
+    namespace Sel{
+        ////////////////////////////////////////////////////////////////////////////////// Declarations
+        /*!
+         * @class Tournament
+         */
+        struct Tournament : SelectionOperator
+        {
+            /** Tournament based selection and survival methods. */
+
+            Tournament(bool surv);
+            
+            ~Tournament();
+
+            /// selection according to the survival scheme of Tournament
+            vector<size_t> select(Population& pop, const MatrixXf& F, 
+                    const Parameters& p, const Data& d);
+            
+            /// survival according to the survival scheme of Tournament
+            vector<size_t> survive(Population& pop, const MatrixXf& F, 
+                    const Parameters& p, const Data& d);
+
+            private:    
+
+                size_t tournament(vector<Individual>& pop, size_t i, size_t j) const;
+        };
+        
+    }
+    
+}
+#endif
