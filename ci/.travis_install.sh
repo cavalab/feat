ef4a68494f0d0c46aaf0afa8d5ce92f409289a8c
lacava <williamlacava@gmail.com>
2020-09-01 15:38:09 -0400

    adds environment for conda install

--- ci/.travis_install.sh
+++ ci/.travis_install.sh
@@ -39,35 +39,44 @@ echo "installing shogun and eigen via conda..."
 wget http://repo.continuum.io/miniconda/Miniconda3-4.7.12.1-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
-
+echo "creating conda environment"
+conda config --set always_yes yes
+conda create -c conda-forge -q -n test-environment python=3.7 shogun-cpp=6.1.3 eigen=3.3.7 json-c=0.12.1-0 cython scikit-learn pandas
+# conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION -c conda-forge shogun-cpp eigen json-c=0.12.1-0 cython scikit-learn pandas
+conda activate test-environment
 
 # conda update --yes conda
-conda install --yes -c conda-forge shogun-cpp eigen
+# conda install --yes -c conda-forge shogun-cpp eigen
 
 # export EIGEN3_INCLUDE_DIR="$(pwd)/eigen-3.3.4/"
-# echo "EIGEN3_INCLUDE_DIR set to $EIGEN3_INCLUDE_DIR"
 # conda install -c conda-forge eigen
 
 # the new version of json-c seems to be missing a fn shogun is linked to;
 # force install of older version
-conda install --yes json-c=0.12.1-0
+# conda install --yes json-c=0.12.1-0
 
-export SHOGUN_LIB=/home/travis/miniconda/lib/
-export SHOGUN_DIR=/home/travis/miniconda/include/
 # commending out the following installs which should be triggered
 # by call to setup.py
-echo "installing cython using conda..."
-conda install --yes cython
+# echo "installing cython using conda..."
+# conda install --yes cython
 
-echo "installing scikit-learn via conda..."
-conda install --yes scikit-learn
+# echo "installing scikit-learn via conda..."
+# conda install --yes scikit-learn
 
-echo "installing pandas via conda..."
-conda install --yes pandas
+# echo "installing pandas via conda..."
+# conda install --yes pandas
 
 echo "printing conda environment"
 conda-env export
 ##########CONDA##############
+
+# set environment variables for eigen and shogun includes
+export EIGEN3_INCLUDE_DIR="$HOME/miniconda/envs/test-environment/include/eigen3/"
+echo "EIGEN3_INCLUDE_DIR set to $EIGEN3_INCLUDE_DIR"
+
+export SHOGUN_LIB=/home/travis/miniconda/envs/test-environment/lib/
+export SHOGUN_DIR=/home/travis/miniconda/envs/test-environment/include/
+
 
 #building and installing google tests
 echo "installing google test"
