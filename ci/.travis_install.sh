aa58633f961a3ea19eed34084500d362f9b47361
lacava <williamlacava@gmail.com>
2020-09-02 15:45:03 -0400

    cleans up install script, uses latest miniconda

--- ci/.travis_install.sh
+++ ci/.travis_install.sh
@@ -1,20 +1,15 @@
-# echo "cmake version:"
-# cmake --version
-# echo "sudo cmake version:"
-# sudo cmake --version
-
-##########CONDA##############
+##############################
+# conda setup and installation
+##############################
 
 echo "installing shogun and eigen via conda..."
-wget http://repo.continuum.io/miniconda/Miniconda3-4.7.12.1-Linux-x86_64.sh -O miniconda.sh
+wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 . "$HOME/miniconda/etc/profile.d/conda.sh"
 hash -r
 echo "creating conda environment"
 conda config --set always_yes yes --set changeps1 no
-# cython=0.29.12
-# conda create -c conda-forge -q -n test-environment python=3.7 shogun-cpp=6.1.3 eigen json-c=0.12.1-0 cython scikit-learn pandas wheel setuptools=41.0.1
 conda env create -f ci/test-environment.yml
 # conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION -c conda-forge shogun-cpp eigen json-c=0.12.1-0 cython scikit-learn pandas
 echo "activating test-environment"
@@ -41,19 +36,18 @@ python --version
 
 echo "cython path is..."
 which cython
-##########CONDA##############
 
 # set environment variables for eigen and shogun includes
 export EIGEN3_INCLUDE_DIR="$HOME/miniconda/envs/feat-env/include/eigen3/"
 echo "EIGEN3_INCLUDE_DIR set to $EIGEN3_INCLUDE_DIR"
-
 export SHOGUN_LIB=/home/travis/miniconda/envs/feat-env/lib/
 echo "SHOGUN_LIB set to $SHOGUN_LIB"
 export SHOGUN_DIR=/home/travis/miniconda/envs/feat-env/include/
 echo "SHOGUN_DIR set to $SHOGUN_DIR"
 
-
+####################################
 #building and installing google tests
+####################################
 echo "installing google test"
 old_path=$(pwd)
 
@@ -72,16 +66,7 @@ cd $old_path; pwd
 ###################
 # feat installation
 ###################
-echo "installing feat..."
-mkdir build;
-cd build; pwd
-echo "which cmake?"
-which cmake
-cmake -DTEST=ON -DEIGEN_DIR=ON -DSHOGUN_DIR=ON ..
-cd ..
-make -C build VERBOSE=1
-
-echo "installing wrapper"
-cd ./python
-python setup.py install
-cd ..
+echo "configuring feat..."
+./configure tests
+echo "install feat..."
+./install tests y
