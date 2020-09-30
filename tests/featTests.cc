2c1086ddd97a864a26ace6d8bc08da2f955d5630
lacava <williamlacava@gmail.com>
2020-09-30 10:57:10 -0400

    starting on tests

--- tests/featTests.cc
+++ tests/featTests.cc
@@ -284,3 +284,29 @@ TEST(Feat, simplification)
     ASSERT_EQ(test_ind3.program.size(), 2);
 }
 
+TEST(Feat, serialization)
+{
+    Feat feat(100, 100, "LinearRidgeRegression", false, 1);
+    feat.set_random_state(666);
+    feat.set_verbosity(2);
+
+    MatrixXf X(7,2); 
+    X << 0,1,  
+         0.47942554,0.87758256,  
+         0.84147098,  0.54030231,
+         0.99749499,  0.0707372,
+         0.90929743, -0.41614684,
+         0.59847214, -0.80114362,
+         0.14112001,-0.9899925;
+
+    X.transposeInPlace();
+    
+    VectorXf y(7); 
+    // y = 2*x1 + 3.x2
+    y << 3.0,  3.59159876,  3.30384889,  2.20720158,  0.57015434,
+             -1.20648656, -2.68773747;
+    std::map<string, std::pair<vector<ArrayXf>, vector<ArrayXf> > > Z;
+    DataRef d(X, y, Z, false);
+
+    // TODO: write this test!
+}
--- tests/individualTests.cc
+++ tests/individualTests.cc
@@ -167,3 +167,61 @@ TEST(Individual, Complexity)
 	a.complexity = 0;
 }
 
+TEST(Individual, serialization)
+{
+	Feat feat(100, 100, "LinearRidgeRegression", false, 1);
+	feat.set_random_state(666);
+	
+    MatrixXf X(4,2); 
+    MatrixXf X_v(3,2); 
+    X << 0,1,  
+         0.47942554,0.87758256,  
+         0.84147098,  0.54030231,
+         0.99749499,  0.0707372;
+    X_v <<  0.90929743, -0.41614684,
+         0.59847214, -0.80114362,
+         0.14112001,-0.9899925;
+
+    X.transposeInPlace();
+    X_v.transposeInPlace();
+
+    VectorXf y(4); 
+    VectorXf y_v(3); 
+    // y = 2*x1 + 3.x2
+    y << 3.0,  3.59159876,  3.30384889,  2.20720158;
+    y_v << 0.57015434, -1.20648656, -2.68773747;
+    
+    std::map<string, std::pair<vector<ArrayXf>, vector<ArrayXf> > > z; 
+
+    feat.params.init(X, y);       
+  
+    feat.set_dtypes(find_dtypes(X));
+            
+    feat.p_ml = make_shared<ML>(); // intialize ML
+    feat.p_pop = make_shared<Population>(feat.params.pop_size);
+    feat.p_eval = make_shared<Evaluation>(feat.params.scorer);
+
+	feat.params.set_terminals(X.rows());
+	
+	Data dt(X, y, z);
+    Data dv(X_v, y_v, z);
+    
+    DataRef d;
+    
+    d.setTrainingData(&dt);
+    d.setValidationData(&dv); 
+        
+    // initial model on raw input
+    feat.initial_model(d);
+                  
+    // initialize population 
+    feat.p_pop->init(feat.best_ind, feat.params);
+
+    //TODO: write this test!
+    // train individuals, save behavior
+    // save individuals
+    // load individuals, predict behavior
+    // check saved behavior matches predictions
+}
+
+
